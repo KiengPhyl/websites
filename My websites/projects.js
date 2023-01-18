6f64aefc54528df74c7c173872bbf80cb73993bb
KiengPhyl <kiengphilemon@gmail.com>
2023-01-18 16:47:15 +0300

    Update of random location

--- My websites/projects.js
+++ My websites/projects.js
@@ -1,7 +1,9 @@
 // Declare three locations where 3 ships are located as 3, 4, and 5.
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+
+let randomLocation = Math.floor(Math.random() * 5);
+let location1 = randomLocation;
+let location2 = location1 + 1;
+let location3 = location2 + 1;
 
 let guess;
 let hits = 0;
